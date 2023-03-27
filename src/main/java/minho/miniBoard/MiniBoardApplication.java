90cf4151fd62b589d38108048f0ecb49a3cabbe3
minho0315 <dlalsgh369@naver.com>
2023-03-27 21:49:17 +0900

    init

---
+++ src/main/java/minho/miniBoard/MiniBoardApplication.java
@@ -0,0 +1,13 @@
+package minho.miniBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniBoardApplication.class, args);
+	}
+
+}
